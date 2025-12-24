42d15c78afa81a1939d3785d8ba9a8b33b30a0a4
GuilhermeSoaress <guilhermesoares278@gmail.com>
2025-12-24 16:52:02 -0300

    chore: setup api client and project structure

---
+++ src/core/api/client.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    appid: API_KEY,
+    units: 'metric',
+    lang: 'en',
+  },
+});
+
+export default apiClient;
